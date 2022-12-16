a9587187103f3756f24536d6f5da34c0474c63e3
SosaRaul <tecraulsosa@gmail.com>
2022-12-16 09:00:57 -0300

    Solved DayThreeAdventOfCode

--- DayThreeAdventOfCode/src/main.cpp
+++ DayThreeAdventOfCode/src/main.cpp
@@ -8,9 +8,8 @@
 
 int main(){
 
-	//cout << "Output Part One! : " << PartOne::getSumOfPriorities("src/input3") << "\n";
-
-
+	cout << "Output Part One! : " << PartOne::getSumOfPriorities("src/input3") << "\n";
+	cout << "Output Part Two! : " << PartTwo::getSumOfPriorities("src/input3");
 	return 0;
 }
 
